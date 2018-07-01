3088b3838bf17c47390b3a82ab418c1e6b1458a6
eliasdabbas <eliasdabbas@gmail.com>
2018-07-02 00:18:29 +0400

    Modify cli functionality to do the packages function

--- dayiwasborn/cli.py
+++ dayiwasborn/cli.py
@@ -5,14 +5,29 @@ import sys
 import click
 
 
+#@click.command()
+#def main(args=None):
+#    """Console script for dayiwasborn."""
+#    click.echo("Replace this message by putting your code into "
+#               "dayiwasborn.cli.main")
+#    click.echo("See click documentation at http://click.pocoo.org/")
+#    return 0
+
+from dayiwasborn.dayiwasborn import dayiwasborn
+
 @click.command()
-def main(args=None):
-    """Console script for dayiwasborn."""
-    click.echo("Replace this message by putting your code into "
-               "dayiwasborn.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument('name', type=str)
+@click.argument('year', type=int)
+@click.argument('month', type=int)
+@click.argument('day', type=int)
+def main(name, year, month, day):
+    click.echo(dayiwasborn(name, year, month, day))
+    if year < 2000:
+        click.echo("You're old!")
     return 0
 
 
+
 if __name__ == "__main__":
+    import sys
     sys.exit(main())  # pragma: no cover
